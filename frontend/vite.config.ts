da6284a5947888eadf3f079d046a53dafb2b36b9
Ithran Beor Turno <turno.ithranbeor7@gmail.com>
2025-11-20 23:24:17 +0800

    Fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  
-  plugins: [
-    react()],
+  plugins: [react()],
+  build: {
+    outDir: 'build', // Render expects this
+    sourcemap: false, // remove if not needed
+    chunkSizeWarningLimit: 600, // raise warning limit, optional
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // all node_modules go to vendor
+            return 'vendor'
+          }
+          if (id.includes('assets')) {
+            // separate assets if needed
+            return 'assets'
+          }
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets')
+    }
+  }
 })
